eafd0bcb4acdfc2a770ef06d45ff277472dd258e
noemi-sitku <152776835+noemi-sitku@users.noreply.github.com>
2024-09-20 16:21:03 +0200

    Add files via upload

---
+++ masodikgyak_jav/masodikgyak_jav/Data/RendelesDbContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using masodikgyak_jav.Models;
+
+namespace masodikgyak_jav.Data;
+
+public partial class RendelesDbContext : DbContext
+{
+    public RendelesDbContext()
+    {
+    }
+
+    public RendelesDbContext(DbContextOptions<RendelesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cim> Cim { get; set; }
+
+    public virtual DbSet<Rendeles> Rendeles { get; set; }
+
+    public virtual DbSet<RendelesTetel> RendelesTetel { get; set; }
+
+    public virtual DbSet<Termek> Termek { get; set; }
+
+    public virtual DbSet<TermekKategoria> TermekKategoria { get; set; }
+
+    public virtual DbSet<Ugyfel> Ugyfel { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=valami.database.windows.net;Initial Catalog=Student;User ID=snoncsi;Password=Snoncs2004valami");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cim>(entity =>
+        {
+            entity.HasKey(e => e.CimId).HasName("PK__CIM__8A3BFC161F122EE5");
+        });
+
+        modelBuilder.Entity<Rendeles>(entity =>
+        {
+            entity.HasKey(e => e.RendelesId).HasName("PK__RENDELES__8DBDC2ADBE1CE4C5");
+
+            entity.Property(e => e.RendelesDatum).HasDefaultValueSql("(getdate())");
+
+            entity.HasOne(d => d.SzallitasiCim).WithMany(p => p.Rendeles)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RENDELES_CIM");
+
+            entity.HasOne(d => d.Ugyfel).WithMany(p => p.Rendeles)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RENDELES_UGYFEL");
+        });
+
+        modelBuilder.Entity<RendelesTetel>(entity =>
+        {
+            entity.HasKey(e => e.TetelId).HasName("PK__RENDELES__18F19F74E29BD96C");
+
+            entity.HasOne(d => d.Rendeles).WithMany(p => p.RendelesTetel)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RENDELES_TETEL_RENDELES");
+
+            entity.HasOne(d => d.Termek).WithMany(p => p.RendelesTetel)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RENDELES_TETEL_TERMEK");
+        });
+
+        modelBuilder.Entity<Termek>(entity =>
+        {
+            entity.HasKey(e => e.TermekId).HasName("PK__TERMEK__05635B29D839F04F");
+
+            entity.HasOne(d => d.Kategoria).WithMany(p => p.Termek).HasConstraintName("FK_TERMEK_TERMEK_KATEGORIA");
+        });
+
+        modelBuilder.Entity<TermekKategoria>(entity =>
+        {
+            entity.HasKey(e => e.KategoriaId).HasName("PK__TERMEK_K__37D210EC7918917C");
+
+            entity.HasOne(d => d.SzuloKategoria).WithMany(p => p.InverseSzuloKategoria).HasConstraintName("FK_TERMEK_KATEGORIA_SzuloKategoria");
+        });
+
+        modelBuilder.Entity<Ugyfel>(entity =>
+        {
+            entity.HasKey(e => e.UgyfelId).HasName("PK__UGYFEL__BD19A3718BB45041");
+
+            entity.HasOne(d => d.Lakcim).WithMany(p => p.Ugyfel).HasConstraintName("FK_UGYFEL_CIM");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ masodikgyak_jav/masodikgyak_jav/Models/TermekKategoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace masodikgyak_jav.Models;
+
+[Table("TERMEK_KATEGORIA")]
+public partial class TermekKategoria
+{
+    [Key]
+    [Column("KategoriaID")]
+    public int KategoriaId { get; set; }
+
+    [StringLength(50)]
+    public string Nev { get; set; } = null!;
+
+    public string? Leiras { get; set; }
+
+    [Column("SzuloKategoriaID")]
+    public int? SzuloKategoriaId { get; set; }
+
+    [InverseProperty("SzuloKategoria")]
+    public virtual ICollection<TermekKategoria> InverseSzuloKategoria { get; set; } = new List<TermekKategoria>();
+
+    [ForeignKey("SzuloKategoriaId")]
+    [InverseProperty("InverseSzuloKategoria")]
+    public virtual TermekKategoria? SzuloKategoria { get; set; }
+
+    [InverseProperty("Kategoria")]
+    public virtual ICollection<Termek> Termek { get; set; } = new List<Termek>();
+}
